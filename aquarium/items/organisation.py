ea42e7de045c817879ed94616c57c5a5f27c6365
Yann Moriaud <yann@moriaud.fr>
2022-10-07 10:21:46 +0200

    Add create member & get member by email

--- aquarium/items/organisation.py
+++ aquarium/items/organisation.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 from ..item import Item
 
 
@@ -7,12 +8,28 @@ class Organisation(Item):
     This class describes an Organisation object child of Item class.
     """
 
+    def get_member_by_email(self, email):
+        """
+        Get an exising member of the organisation by his/her email
+
+        :returns:   User object
+        :rtype:     :class:`~aquarium.items.user.User`
+        """
+        member = None
+        members = self.get_all_members()
+        filtered = [member for member in members if member.data.email == email]
+
+        if (len(filtered) > 0):
+            member = filtered[0]
+
+        return member
+
     def get_all_members(self):
         """
         Gets all members of the organisation
 
         :returns:   List of User object
-        :rtype:     List of :class:`~aquarium.items.task.User`
+        :rtype:     List of :class:`~aquarium.items.user.User`
         """
 
         result=self.do_request('GET', 'organisations/{0}/members/all'.format(self._key))
@@ -24,7 +41,7 @@ class Organisation(Item):
         Gets all active members of the organisation
 
         :returns:   List of User object
-        :rtype:     List of :class:`~aquarium.items.task.User`
+        :rtype:     List of :class:`~aquarium.items.user.User`
         """
 
         result=self.do_request('GET', 'organisations/{0}/members/active'.format(self._key))
@@ -36,9 +53,29 @@ class Organisation(Item):
         Gets all inactive members of the organisation
 
         :returns:   List of User object
-        :rtype:     List of :class:`~aquarium.items.task.User`
+        :rtype:     List of :class:`~aquarium.items.user.User`
         """
 
         result=self.do_request('GET', 'organisations/{0}/members/inactive'.format(self._key))
         result=[self.parent.user(user) for user in result]
         return result
+
+    def create_member(self, email, name=None):
+        """
+        Create a new member in your organisation
+
+        :param      email:  The email of the new member
+        :type       type:  string
+        :param      name:  The name of the new member
+        :type       type:  string, optional
+
+        :returns:   User object
+        :rtype:     :class:`~aquarium.items.user.User`
+        """
+
+        payload = dict(email=email, name=name)
+        member = self.do_request(
+            'POST', 'organisation/{0}/createMember'.format(self._key), data=json.dumps(payload))
+
+        member = self.parent.cast(member)
+        return member
