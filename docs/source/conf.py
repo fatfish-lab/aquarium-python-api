d0fceeff70a221590e7377425599fe49e8cce984
Yann Moriaud <yann@moriaud.fr>
2023-01-12 09:12:32 +0100

    Bump to v1.0.19

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright='2022, Fatfish Lab'
 author='Fatfish Lab'
 
 # The full version, including alpha/beta/rc tags
-release='1.0.18'
+release='1.0.19'
 
 
 # -- General configuration ---------------------------------------------------
